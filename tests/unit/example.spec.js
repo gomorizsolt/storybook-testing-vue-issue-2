1bc2ed2833cc1e692d8c78bbf49ed98006c1c9cb
Zsolt Gömöri <zsgomoridev@icloud.com>
2021-07-16 09:51:00 +0200

    update sample test, add runtimeCompiler config

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,15 +1,15 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import { composeStories } from '@storybook/testing-vue';
 import * as buttonStories from '../../src/stories/Button.stories';
 
 const { Primary } = composeStories(buttonStories)
 
 describe('Button', () => {
-  it('renders props.msg when passed', () => {
-    const buttonWrapper = shallowMount(Primary(), {
+  it('renders props.label when passed', () => {
+    const buttonWrapper = mount(Primary(), {
       propsData: {
         label: 'Apply filters'
-      }
+      },
     });
 
     expect(buttonWrapper.text()).toEqual('Apply filters');
